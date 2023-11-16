7e36a201d8c81974aba5b55d4459a16511b39347
Andreas Molzer <andreas.molzer@gmx.de>
2023-11-16 20:42:30 +0100

    Demonstrate Reader wrapper

--- flexible-io/src/reader.rs
+++ flexible-io/src/reader.rs
@@ -1,5 +1,18 @@
 use std::io::{BufRead, Read, Seek};
 
+
+/// A reader, which can _dynamically_ implement IO traits.
+///
+/// The struct comes with a number of setter methods. The call to these requires proof to the
+/// compiler that the bound is met, inserting the vtable from the impl instance. Afterward, the
+/// bound is not required by any user. Using the (mutable) getters recombines the vtable with the
+/// underlying value.
+///
+/// Note that the value can not be unsized (`dyn` trait) itself. This may be fixed at a later point
+/// to make the reader suitable for use in embedded. In particular, the double indirection of
+/// instantiating with `R = &mut dyn Read` wouldn't make sense as the setters would not be usable,
+/// their bounds can never be met. And combining traits into a large dyn-trait is redundant as it
+/// trait-impls become part of the static validity requirement again.
 pub struct Reader<R: Read> {
     inner: R,
     seek: Option<*mut dyn Seek>,
@@ -7,6 +20,25 @@ pub struct Reader<R: Read> {
 }
 
 impl<R: Read> Reader<R> {
+    pub fn new(reader: R) -> Self {
+        Reader {
+            inner: reader,
+            seek: None,
+            buf: None,
+        }
+    }
+
+    /// Provide access to the underlying reader.
+    pub fn get_ref(&self) -> &R {
+        &self.inner
+    }
+
+    /// Provide mutable access to the underlying reader.
+    pub fn get_mut(&mut self) -> &mut R {
+        &mut self.inner
+    }
+
+    /// Insert the vtable for the `BufRead` trait.
     pub fn set_buf<'lt>(&mut self)
     where
         R: BufRead + 'lt,
@@ -17,35 +49,39 @@ impl<R: Read> Reader<R> {
         self.buf = Some(unsafe { core::mem::transmute::<_, *mut (dyn BufRead + 'static)>(vtable) });
     }
 
-    pub fn set_seek<'lt>(&mut self)
+    /// Insert the vtable for the `Seek` trait.
+    ///
+    /// This call itself requires proof to the compiler that the bound is met, inserting the vtable
+    /// from the impl instance. The bound is not required by any user of that inserted vtable.
+    pub fn set_seek(&mut self)
     where
-        R: Seek + 'lt,
+        R: Seek,
     {
-        let vtable = (&mut self.inner) as &mut (dyn Seek + 'lt) as *mut (dyn Seek + 'lt);
+        let vtable = (&mut self.inner) as &mut (dyn Seek + '_) as *mut (dyn Seek + '_);
         // Safety: Transmuting pointer-to-pointer, and they only differ by lifetime. Types must not
         // be specialized on lifetime parameters.
         self.seek = Some(unsafe { core::mem::transmute::<_, *mut (dyn Seek + 'static)>(vtable) });
     }
 
-    pub fn buf(&self) -> Option<&(dyn BufRead + '_)> {
+    pub fn as_buf(&self) -> Option<&(dyn BufRead + '_)> {
         let ptr = &self.inner as *const R;
         let local = ptr.with_metadata_of(self.buf?);
         Some(unsafe { &* local })
     }
 
-    pub fn buf_mut(&mut self) -> Option<&mut (dyn BufRead + '_)> {
+    pub fn as_buf_mut(&mut self) -> Option<&mut (dyn BufRead + '_)> {
         let ptr = &mut self.inner as *mut R;
         let local = ptr.with_metadata_of(self.buf?);
         Some(unsafe { &mut* local })
     }
 
-    pub fn seek(&self) -> Option<&(dyn Seek + '_)> {
+    pub fn as_seek(&self) -> Option<&(dyn Seek + '_)> {
         let ptr = &self.inner as *const R;
         let local = ptr.with_metadata_of(self.seek?);
         Some(unsafe { &* local })
     }
 
-    pub fn seek_mut(&mut self) -> Option<&mut (dyn Seek + '_)> {
+    pub fn as_seek_mut(&mut self) -> Option<&mut (dyn Seek + '_)> {
         let ptr = &mut self.inner as *mut R;
         let local = ptr.with_metadata_of(self.seek?);
         Some(unsafe { &mut* local })
---
+++ flexible-io/tests/motivation.rs
@@ -0,0 +1,127 @@
+//! Demonstrates an actual use case.
+//!
+//! In many cases, IO can be performed in ultimately equivalent ways. Seeking forward for instance
+//! can be emulated by simply reading as many bytes as need to be skipped. The library address that
+//! case, which is implemented in `read_with_skip`.
+//!
+//! Similarly, `BufRead` can be more efficient yet requiring it will force some callers into
+//! double-buffering and rob them of the *choice* of buffering. This is demonstrated in
+//! `read_TODO`.
+use std::io::{Read, SeekFrom};
+use flexible_io::Reader;
+
+
+#[test]
+fn motivating_case() {
+    {
+        let mut untapped: &[u8] = b"Hello, world!";
+        let reader = Reader::new(&mut untapped);
+        let mut buffer = vec![];
+        let report = read_with_skip(reader, 7, &mut buffer).unwrap();
+        assert_eq!(buffer, b"world!");
+        assert_eq!(
+            report.num_seek, 0,
+            "Obvious can't seek, we didn't tell that it can"
+        );
+
+        assert!(
+            report.num_read >= 3,
+            "Seek and read took two reads. We know slices fulfill the whole request if possible. Then a third read zeros to tell the reader that the slice is EOF."
+        );
+    }
+
+    {
+        let mut untapped: &[u8] = b"Hello, world!";
+        let tapped = std::io::Cursor::new(&mut untapped);
+        let mut reader = Reader::new(tapped);
+        reader.set_seek();
+        let mut buffer = vec![];
+        let report = read_with_skip(reader, 7, &mut buffer).unwrap();
+        assert_eq!(buffer, b"world!");
+        assert_eq!(
+            report.num_seek, 1,
+            "Offset was resolved via seek, we didn't tell that it can"
+        );
+
+        assert!(
+            report.num_read >= 2,
+            "Read took two reads. We know slices fulfill the whole request if possible. Then a third read zeros to tell the reader that the slice is EOF."
+        );
+    }
+}
+
+#[derive(Default)]
+pub struct IoReport {
+    num_seek: u32,
+    num_read: u32,
+}
+
+/// This operation skips N bytes from the stream, returns the rest read to end.
+///
+/// It works on *all* kinds of streams, but is optimized for seekable streams. However, the
+/// direct caller need not worry about this in the interface. Only `Read` is a hard
+/// requirement. Another third party can make the choice whether there is a vtable for `Seek`
+/// or not with our caller just passing this encapsulation on.
+pub fn read_with_skip<R: Read>(
+    mut file: Reader<R>,
+    skip: u64,
+    buffer: &mut Vec<u8>,
+)
+    -> Result<IoReport, std::io::Error>
+{
+    let mut report = IoReport::default();
+
+    if let Some(seekable) = file.as_seek_mut() {
+        let mut skip: u64 = skip;
+        while skip > 0 {
+            let offset = i64::try_from(skip).unwrap_or(i64::MAX);
+            seekable.seek(SeekFrom::Current(skip as i64))?;
+            report.num_seek += 1;
+            skip -= offset as u64;
+        }
+    } else {
+        // No optimization. Use a loop to throw away all these bytes.
+        let mut skip: u64 = skip;
+
+        let mut buffer = Box::new([0; 1 << 14]);
+        while skip > 0 {
+            let bound = usize::try_from(skip).unwrap_or(usize::MAX);
+            let exact_read = buffer.len().min(bound);
+            let actual = file.get_mut().read(&mut buffer[..exact_read])?;
+            report.num_read += 1;
+
+            if actual == 0 {
+                return Err(std::io::ErrorKind::UnexpectedEof)?;
+            }
+
+            skip -= actual as u64;
+        }
+    }
+
+    let reader = file.get_mut();
+    let inner = read_to_end(reader, buffer)?;
+    report.num_seek += inner.num_seek;
+    report.num_read += inner.num_read;
+
+    Ok(report)
+}
+
+// Poly-fill for std::io::Read::read_to_end (default), with a report on the number of actual reads
+// that were used; for the purpose of IO-accounting.
+fn read_to_end<R: Read>(
+    reader: &mut R,
+    buffer: &mut Vec<u8>,
+) -> Result<IoReport, std::io::Error> {
+    struct Plug<R>(pub R, IoReport);
+
+    impl<R: Read> std::io::Read for Plug<R> {
+        fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
+            self.1.num_read += 1;
+            self.0.read(buf)
+        }
+    }
+
+    let mut plug = Plug(reader, IoReport::default());
+    plug.read_to_end(buffer)?;
+    Ok(plug.1)
+}
